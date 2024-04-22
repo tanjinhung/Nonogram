fbd34aa6824d370b96fb109e6294462ec97a1e66
tanjinhung <tanjinhung2013@gmail.com>
2024-04-22 10:59:20 +0900

    modified:   learn/learn_windows.c 	modified:   learn/learn_windows.exe

--- learn/learn_windows.c
+++ learn/learn_windows.c
@@ -1,12 +1,21 @@
 #include <windows.h>
 
-const char g_szClassName[] = "myWindowClass";
+const char CLASS_NAME[] = "Sample Window Class";
 
 // Step 4: the Window Procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
+    case WM_LBUTTONDOWN:
+        {
+            char szFILENAME[MAX_PATH];
+            HINSTANCE hInstance = GetModuleHandle(NULL);
+            
+            GetModuleFileName(hInstance, szFILENAME, MAX_PATH);
+            MessageBox(hwnd, szFILENAME, "This program is:", MB_OK | MB_ICONINFORMATION);
+        }
+        break;
     case WM_CLOSE:
         DestroyWindow(hwnd);
         break;
@@ -19,7 +28,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char* lpCmdLine, int nCmdShow)
 {
     WNDCLASSEX wc;
     HWND hwnd;
@@ -36,7 +45,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.lpszMenuName = NULL;
-    wc.lpszClassName = g_szClassName;
+    wc.lpszClassName = CLASS_NAME;
     wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
     if (!RegisterClassEx(&wc))
@@ -47,11 +56,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     // Step 2: Creating the Window
     hwnd = CreateWindowEx(
-        WS_EX_CLIENTEDGE,
-        g_szClassName,
-        "The title of my window",
+        0,
+        CLASS_NAME,
+        "Title",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
         NULL, NULL, hInstance, NULL);
 
     if (hwnd == NULL)
