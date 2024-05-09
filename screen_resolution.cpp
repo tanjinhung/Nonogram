eb4c7e0f6bdbe2b782874b51c5662135ef13362d
tanjinhung <tanjinhung2013@gmail.com>
2024-05-09 14:42:48 +0900

    modified:   .gitignore 	new file:   screen_resolution.cpp

---
+++ screen_resolution.cpp
@@ -0,0 +1,15 @@
+#include "windows.h"
+#include <windows.h>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    DEVMODE dm = {0};
+    dm.dmSize = sizeof(dm);
+    for (int iModeNum = 0; EnumDisplaySettings(NULL, iModeNum, &dm) != 0; iModeNum++)
+    {
+        std::cout << "Mode #" << iModeNum << " = " << dm.dmPelsWidth << "x" << dm.dmPelsHeight << std::endl;
+    }
+    
+    return 0;
+}
