f08e0efc668b1ce24b6efbf5ff5feba59d72d0a1
Tan Jin Hung <74574144+tanjinhung@users.noreply.github.com>
2024-05-09 15:03:00 +0900

    0503 (#2)
    * modified:   main.cpp
    
    * modified:   diagram/diagram.drawio
    
    * changed: draw.io -> svg
    
    * modified:   .gitignore
    	new file:   screen_resolution.cpp
    
    * modified:   .gitignore
    	modified:   learn/learn_windows.exe

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <windows.h>
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg)
+    {
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;    
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdLine) {
-    
+    int sWidth = GetSystemMetrics(SM_CXSCREEN);
+    int sHeight = GetSystemMetrics(SM_CYSCREEN);
 }
