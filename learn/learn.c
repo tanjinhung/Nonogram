8a229e22a9070b5e0c02627a359b9d4f622f39db
tanjinhung <tanjinhung2013@gmail.com>
2024-07-29 17:06:41 +0900

    deleted: unwanted files

--- learn/learn.c
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    MessageBox(NULL, "Goodbye, Cruel World!", "Note", MB_OK);
-    return 0;
-}
